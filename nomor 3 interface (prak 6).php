0254c629ac07232f168467b3f2891a8769202bde
ashaantania <100705942+ashaantania@users.noreply.github.com>
2022-10-20 21:47:47 +0700

    Add files via upload

---
+++ nomor 3 interface (prak 6).php
@@ -0,0 +1,14 @@
+<!-- Asha Antania Anjani_21091397068-->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
---
+++ nomor 4 interface (prak 6).php
@@ -0,0 +1,16 @@
+<!--- Nama    : Asha Antania Anjani
+      NIM     : 21091397068
+      Kelas   : 2021 B--->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
